4466cd3868322a9c54b68a3df7863771d278a2ad
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-10-26 01:22:51 +0700

    update cek-tr.sh

--- xray/trojan/cek-tr.sh
+++ xray/trojan/cek-tr.sh
@@ -17,7 +17,6 @@ IZIN=$( curl ipinfo.io/ip | grep $MYIP )
 if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
-echo -e "${NC}${RED}Permission Denied!${NC}";
 exit 0
 fi
 clear
