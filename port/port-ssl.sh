f368bfa4644cc33b0cc6cdf0a7f6b13c8667b9f7
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-10-26 00:48:34 +0700

    update port-ssl.sh

--- port/port-ssl.sh
+++ port/port-ssl.sh
@@ -8,9 +8,6 @@ NC='\e[0m'
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
-
-
-
 clear
 ssl="$(cat /etc/stunnel/stunnel.conf | grep -i accept | head -n 2 | cut -d= -f2 | sed 's/ //g' | tr '\n' ' ' | awk '{print $1}')"
 ssl2="$(cat /etc/stunnel/stunnel.conf | grep -i accept | head -n 2 | cut -d= -f2 | sed 's/ //g' | tr '\n' ' ' | awk '{print $2}')"
