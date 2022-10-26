56595df1f5bc2010852c9cf9a8b289ecc7166632
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-10-26 15:53:46 +0700

    update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -246,12 +246,12 @@ echo 'Config file is at /usr/local/ddos/ddos.conf'
 echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 
 # banner /etc/issue.net
-sleep 1
-echo -e "[ ${green}INFO$NC ] Settings banner"
-wget -q -O /etc/issue.net "https://raw.githubusercontent.com/nanotechid/sv/script/rex/banner/issue.net"
-chmod +x /etc/issue.net
-echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
-sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
+#sleep 1
+#echo -e "[ ${green}INFO$NC ] Settings banner"
+#wget -q -O /etc/issue.net "https://raw.githubusercontent.com/nanotechid/sv/script/rex/banner/issue.net"
+#chmod +x /etc/issue.net
+#echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
+#sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
 #install bbr dan optimasi kernel
 wget https://raw.githubusercontent.com/nanotechid/sv/script/ssh/bbr.sh && chmod +x bbr.sh && ./bbr.sh
