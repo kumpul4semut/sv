2bdd4bca09600664652eeb95e22b292ce97d0e2c
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-10-29 06:58:38 +0700

    update trialvmess.sh

--- xray/vmess/trialvmess.sh
+++ xray/vmess/trialvmess.sh
@@ -1,5 +1,5 @@
 domain=$(cat /etc/xray/domain)
-dns=($cat /root/dns)
+dns=$(cat /root/dns)
 tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
 user=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
