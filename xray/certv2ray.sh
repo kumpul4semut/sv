76340eef32c127b2802b8cdc63460b5434892779
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-10-29 16:56:07 +0700

    update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -91,7 +91,7 @@ cfemail=$(cat /etc/cfemail)
 export CF_Key=$cfkey
 export CF_Email=$cfemail
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue --dns dns_cf -d $domain -d *.$domain --server zerossl -k ec-256 --debug
+/root/.acme.sh/acme.sh --issue --dns dns_cf -d $domain -d *.$domain --server letsencrypt -k ec-256 --debug
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchain-file /etc/xray/xray.crt --key-file /etc/xray/xray.key --ecc
 echo -e "[ ${green}INFO${NC} ] Renew cert done... " 
 sleep 2
