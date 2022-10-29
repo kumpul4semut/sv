70dd9ef920fba895f526f6968b35c722fcb3420e
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-10-30 01:44:02 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -307,7 +307,9 @@ echo "   - Vless TLS		: 443" | tee -a log-install.txt
 echo "   - Vless None TLS	: 80" | tee -a log-install.txt
 echo "   - Trojan GRPC		: 443" | tee -a log-install.txt
 echo "   - Trojan WS		: 443" | tee -a log-install.txt
-echo "   - Sodosok WS/GRPC    : 443" | tee -a log-install.txt
+echo "   - Trojan XTLS		: 443" | tee -a log-install.txt
+echo "   - Shadowsocks WS    : 443" | tee -a log-install.txt
+echo "   - Shadowsocks GRPC    : 443" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone		: Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
