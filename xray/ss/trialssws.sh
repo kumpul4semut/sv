fa692c50dbb3a933f897f1ba60cf95c10d6ba12e
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-10-29 07:41:38 +0700

    update trialssws.sh

--- xray/ss/trialssws.sh
+++ xray/ss/trialssws.sh
@@ -14,8 +14,8 @@ echo $cipher:$uuid > /tmp/log
 shadowsocks_base64=$(cat /tmp/log)
 echo -n "${shadowsocks_base64}" | base64 > /tmp/log1
 shadowsocks_base64e=$(cat /tmp/log1)
-shadowsockslink="ss://${shadowsocks_base64e}@isi_bug_disini:$tls?path=ss-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
-shadowsockslink1="ss://${shadowsocks_base64e}@${domain}:$tls?mode=gun&security=tls&type=grpc&serviceName=ss-grpc&sni=bug.com#${user}"
+shadowsockslink="ss://${shadowsocks_base64e}@isi_bug_disini:$tls?path=ss-ws&security=tls&host=${dns}&type=ws&sni=${dns}#${user}"
+shadowsockslink1="ss://${shadowsocks_base64e}@${dns}:$tls?mode=gun&security=tls&type=grpc&serviceName=ss-grpc&sni=bug.com#${user}"
 systemctl restart xray > /dev/null 2>&1
 service cron restart > /dev/null 2>&1
 clear
