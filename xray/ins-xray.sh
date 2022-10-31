63748a1965b658d6373dde6def74383165170028
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-11-01 06:19:11 +0700

    update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -34,6 +34,54 @@ apt -y install chrony
 apt install zip -y
 apt install curl pwgen openssl netcat cron -y
 
+# install fail2ban
+apt -y install fail2ban
+
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+
+# blokir torrent
+apt install netfilter-persistent -y
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+
 # install xray
 sleep 1
 echo -e "[ ${green}INFO$NC ] Downloading & Installing xray core"
@@ -81,7 +129,6 @@ if ! grep -q 'ssl_renew.sh' /var/spool/cron/crontabs/root;then (crontab -l;echo 
 
 # set uuid
 uuid=$(cat /proc/sys/kernel/random/uuid)
-ss22=$(openssl rand -base64 32)
 # xray config
 cat > /etc/xray/config.json << END
 {
@@ -507,10 +554,8 @@ wget -O m-trojan "https://raw.githubusercontent.com/nanotechid/sv/script/menu/m-
 
 # menu system
 wget -O m-system "https://raw.githubusercontent.com/nanotechid/sv/script/menu/m-system.sh"
-#wget -O m-cf "https://raw.githubusercontent.com/nanotechid/sv/script/menu/m-cf.sh"
 wget -O m-domain "https://raw.githubusercontent.com/nanotechid/sv/script/menu/m-domain.sh"
 wget -O add-host "https://raw.githubusercontent.com/nanotechid/sv/script/ssh/add-host.sh"
-#wget -O port-change "https://raw.githubusercontent.com/nanotechid/sv/script/port/port-change.sh"
 wget -O certv2ray "https://raw.githubusercontent.com/nanotechid/sv/script/xray/certv2ray.sh"
 wget -O m-webmin "https://raw.githubusercontent.com/nanotechid/sv/script/menu/m-webmin.sh"
 wget -O speedtest "https://raw.githubusercontent.com/nanotechid/sv/script/ssh/speedtest_cli.py"
