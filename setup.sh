d964ad78dfcc655b088a31365ab074e2d2800af5
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-11-01 06:35:21 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -277,29 +277,18 @@ fi
 if [ ! -f "/etc/log-create-user.log" ]; then
 echo "Log All Account " > /etc/log-create-user.log
 fi
-history -c
-serverV=$( curl -sS https://raw.githubusercontent.com/nanotechid/sv/script/rex/version  )
-echo $serverV > /opt/.ver
-aureb=$(cat /home/re_otm)
-b=11
-if [ $aureb -gt $b ]
-then
-gg="PM"
-else
-gg="AM"
-fi
+
 curl -sS ifconfig.me > /etc/myipvps
 echo " "
 echo "------------------------------------------------------------"
 echo ""
-echo ""
 echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH		: 22"  | tee -a log-install.txt
-echo "   - SSH Websocket	: 80" | tee -a log-install.txt
-echo "   - SSH SSL Websocket	: 443" | tee -a log-install.txt
-echo "   - Stunnel4		: 222, 777" | tee -a log-install.txt
-echo "   - Dropbear		: 109, 143" | tee -a log-install.txt
-echo "   - Badvpn		: 7100-7900" | tee -a log-install.txt
+#echo "   - OpenSSH		: 22"  | tee -a log-install.txt
+#echo "   - SSH Websocket	: 80" | tee -a log-install.txt
+#echo "   - SSH SSL Websocket	: 443" | tee -a log-install.txt
+#echo "   - Stunnel4		: 222, 777" | tee -a log-install.txt
+#echo "   - Dropbear		: 109, 143" | tee -a log-install.txt
+#echo "   - Badvpn		: 7100-7900" | tee -a log-install.txt
 echo "   - Nginx		: 81" | tee -a log-install.txt
 echo "   - Vmess TLS		: 443" | tee -a log-install.txt
 echo "   - Vmess None TLS	: 80" | tee -a log-install.txt
@@ -311,22 +300,6 @@ echo "   - Trojan XTLS		: 443" | tee -a log-install.txt
 echo "   - Shadowsocks WS       : 443" | tee -a log-install.txt
 echo "   - Shadowsocks GRPC     : 443" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone		: Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
-echo "   - Fail2Ban		: [ON]"  | tee -a log-install.txt
-echo "   - Dflate		: [ON]"  | tee -a log-install.txt
-echo "   - IPtables		: [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot		: [ON]"  | tee -a log-install.txt
-echo "   - IPv6			: [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On	: $aureb:00 $gg GMT +7" | tee -a log-install.txt
-echo "   - AutoKill Multi Login User" | tee -a log-install.txt
-echo "   - Auto Delete Expired Account" | tee -a log-install.txt
-echo "   - Fully automatic script" | tee -a log-install.txt
-echo "   - VPS settings" | tee -a log-install.txt
-echo "   - Admin Control" | tee -a log-install.txt
-echo "   - Change port" | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
-echo ""
 echo ""
 echo "------------------------------------------------------------"
 echo -e ""
